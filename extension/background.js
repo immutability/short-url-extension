cb48af3984b208217286d32c4f72af7182482bb3
Radoslav Bielik <immutability@bielik.org>
2019-11-24 22:48:59 +0100

    formatting fixes for consistency (4 spaces indent size)

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,19 @@
 // When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // That fires for supported eBay URLs
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlMatches: 'ebay\.(com|de|co\.uk|com\.au|at|be|ca|fr|ie|it|com\.hk|com\.my|com\.sg|nl|ph|pl|es|ch)\/itm' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
+chrome.runtime.onInstalled.addListener(function () {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                // That fires for supported eBay URLs
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: {urlMatches: 'ebay\.(com|de|co\.uk|com\.au|at|be|ca|fr|ie|it|com\.hk|com\.my|com\.sg|nl|ph|pl|es|ch)\/itm'},
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
+    });
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,60 +1,60 @@
 function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
+    chrome.tabs.query(queryInfo, function (tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
 
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+        console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
+        callback(url);
+    });
 }
 
 function renderSuccess(url) {
-	$('#success').append(`Short URL copied to clipboard:<br/><a href="${url}" target="_blank">${url}</a>`);
-	$('#success').show();
+    $('#success').append(`Short URL copied to clipboard:<br/><a href="${url}" target="_blank">${url}</a>`);
+    $('#success').show();
 }
 
 function renderFailure(message) {
-	$('#failure').append(
-`Could not find Item ID.<br/>If you believe this is an error, please report the full URL using the \
+    $('#failure').append(
+        `Could not find Item ID.<br/>If you believe this is an error, please report the full URL using the \
 <a href="${getExtensionHomepage()}" target="_blank">Extension Support Page</a>`);
-	$('#failure').show();
+    $('#failure').show();
 }
 
 function getExtensionHomepage() {
-	return 'https://chrome.google.com/webstore/detail/' + chrome.runtime.id;
+    return 'https://chrome.google.com/webstore/detail/' + chrome.runtime.id;
 }
 
 function parseTheUrl(url) {
-	var urlPattern = /ebay\.(com|de|co\.uk|com\.au|at|be|ca|fr|ie|it|com\.hk|com\.my|com\.sg|nl|ph|pl|es|ch)\/itm/
-	if(urlPattern.test(url)) {
-		var idExt = /\/\d+\?/.exec(url)[0];
-		var id = /\d+/.exec(idExt)[0];
-		return 'http://www.' + urlPattern.exec(url)[0] + '/' + id;
-	}
-	else
+    var urlPattern = /ebay\.(com|de|co\.uk|com\.au|at|be|ca|fr|ie|it|com\.hk|com\.my|com\.sg|nl|ph|pl|es|ch)\/itm/;
+    if (urlPattern.test(url)) {
+        var idExt = /\/\d+\?/.exec(url)[0];
+        var id = /\d+/.exec(idExt)[0];
+        return 'http://www.' + urlPattern.exec(url)[0] + '/' + id;
+    } else {
 		return '';
+	}
 }
 
 function copyToClipboard(url) {
-	var sandbox = $('#sandbox').val(url).select();
-	document.execCommand('copy');
-	sandbox.val('');
+    var sandbox = $('#sandbox').val(url).select();
+    document.execCommand('copy');
+    sandbox.val('');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-	var shortUrl = parseTheUrl(url);
-	if(shortUrl.length > 0) {
-		copyToClipboard(shortUrl);
-		renderSuccess(shortUrl);
-	} else {
-		renderFailure();
-	}
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    getCurrentTabUrl(function (url) {
+        var shortUrl = parseTheUrl(url);
+        if (shortUrl.length > 0) {
+            copyToClipboard(shortUrl);
+            renderSuccess(shortUrl);
+        } else {
+            renderFailure();
+        }
+    });
 });
